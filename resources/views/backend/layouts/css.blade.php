daf09fc5503cdee1347526c467260355db86344d
dasraju <rajud6140@gmail.com>
2022-08-21 00:29:02 +0600

    first commit

---
+++ resources/views/backend/layouts/css.blade.php
@@ -0,0 +1,7 @@
+  <!-- General CSS Files -->
+  <link rel="stylesheet" href="{{asset('assets/backend')}}/css/app.min.css">
+  <!-- Template CSS -->
+  <link rel="stylesheet" href="{{asset('assets/backend')}}/css/style.css">
+  <link rel="stylesheet" href="{{asset('assets/backend')}}/css/components.css">
+  <!-- Custom style CSS -->
+  <link rel="stylesheet" href="{{asset('assets/backend')}}/css/custom.css">
