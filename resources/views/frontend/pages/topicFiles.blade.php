eb60cab5e0e6b4b77f67b16b21e89a79a913affd
dasraju <rajud140@gmail.com>
2022-11-20 02:11:21 +0600

    topic file change again

--- resources/views/frontend/pages/topicFiles.blade.php
+++ resources/views/frontend/pages/topicFiles.blade.php
@@ -1,6 +1,6 @@
 @extends('frontend.master')
 @section('content')
-<<<<<<< HEAD
+
   <!-------Revision Course Name--------->
     <div class="rev_name">
       <div class="container-fluid">
@@ -24,46 +24,9 @@
                   <button>
                     <i class="fa-solid fa-magnifying-glass"></i>
                   </button>
-=======
 
-    <!-- About Start -->
-    <div class="container-xxl py-5">
-        <div class="container">
-            <div class="text-center wow fadeInUp" data-wow-delay="0.1s">
-                <h6 class="section-title bg-white text-center text-primary px-3">Topic</h6>
-                <h1 class="mb-5">{{ $topic->name }}</h1>
-            </div>
-             <div class="row g-4">
-                
-                <div class="col-lg-6 col-md-6 wow fadeInUp" data-wow-delay="0.1s">
-                    <div class="team-item bg-light">
-                        <div class="overflow-hidden">
-                            <img class="img-fluid" src="img/team-1.jpg" alt="">
-                        </div>
-                        <div class="position-relative d-flex justify-content-center" style="margin-top: -23px;">
 
-                        </div>
-                        <div class="text-center p-4">
-                            <h5 class="mb-0">Pdf Notes</h5>
-                            <div class="row">
-                                @foreach ($notes as $note )
-                                <div class="col-sm-6">
-                                    <a href="#"  class="mb-0"><i class="fa fa-arrow-right text-primary me-2"></i>{{$note->title}}</a>
-
-                                    @if($note->view == '1')
-                                    <a href="#"  class="mb-0 mr-2"><i class="fa fa-eye text-success"></i></a>
-                                    @endif
-                                    @if($note->download == '1')
-                                    <a href="#"  class="mb-0 ml-2"><i class="fa fa-download text-danger"></i></a>
-                                    @endif
-
-                                </div>
-                                @endforeach
-                            </div>
-
-                        </div>
-                    </div>
->>>>>>> 4d27fa731fa59aac15291509546276db5bd69b24
+ 
                 </div>
               </div>
             </div>
